aede3791ac0f49bd2aed48abce0ec49c24e996f1
David Lara <david.lara.moreira@udla.edu.ec>
2023-05-17 15:41:33 -0500

    Segundo commit

--- Determinante matriz/diagonalmatriz.c
+++ Determinante matriz/diagonalmatriz.c
@@ -1,42 +1,33 @@
-#include<stdio.h>
-int main(){
-    int mat1[2][3]={0,0,0,0,0,0}; //[filas][columnas]
-    int mat2[2][3]={0,0,0,0,0,0};
-    int resul[2][3]={0,0,0,0,0,0};
-    for(int i=0;i<2;i++){ //i=filas
-        for(int j=0;j<3;j++){ //j=columna
-            scanf("%d",&mat1[i][j]);
-        } 
+#include <stdio.h>
+int main()
+{
+    int filas, columnas;
+    printf("Ingrese el numero de filas que desea para su matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el numero de columnas que desea para su matriz: ");
+    scanf("%d", &columnas);
+    int matriz[filas][columnas];
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz[i][j] = 0;
+        }
     }
-    for(int i=0;i<2;i++){ 
-        for(int j=0;j<3;j++){
-            scanf("%d",&mat2[i][j]);
-        } 
-    }
-    printf("Su primera matriz es:\n");
-    for(int i=0;i<2;i++){ 
-        for(int j=0;j<3;j++){ 
-            printf("%d  ",mat1[i][j]);
-        } 
-        printf("\n");
-    }
-    printf("Su segunda matriz es:\n");
-    for(int i=0;i<2;i++){ 
-        for(int j=0;j<3;j++){
-            printf("%d  ",mat2[i][j]);
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf("%d", matriz[i][j]);
         }
         printf("\n");
     }
-    for(int i=0;i<2;i++){ 
-        for(int j=0;j<3;j++){
-            resul[i][j]=mat1[i][j]+mat2[i][j];
-        } 
-    }
-    printf("Su matriz resultante es:\n");
-    for(int i=0;i<2;i++){ 
-        for(int j=0;j<3;j++){
-            printf("%d  ",resul[i][j]);
-        } 
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf("%d", matriz[i][j]);
+        }
         printf("\n");
     }
     return 0;

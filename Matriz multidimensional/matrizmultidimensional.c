ec1944fff2321448668946347b9870f414c5a58d
David Lara <david.lara.moreira@udla.edu.ec>
2023-05-22 15:52:33 -0500

    Primer commit

---
+++ Matriz multidimensional/matrizmultidimensional.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+    int filas, columnas, num;
+    printf("Ingrese el numero de matrices que desea: ");
+    scanf("%d", &num);
+    printf("Ingrese el numero de filas que desea para su matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el numero de columnas que desea para su matriz: ");
+    scanf("%d", &columnas);
+    int matriz[filas][columnas][num];
+    for (int n = 0; n < num; n++)
+    {
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                matriz[n][i][j] = 0;
+            }
+        }
+    }
+for (int n = 0; n < num; n++)
+{
+if (n==num-1)
+{
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                matriz[n][i][j] = 1;
+            }
+        }
+}
+
+}
+    for (int n = 0; n < num; n++)
+    {
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                printf("%d", matriz[n][i][j]);
+            }
+            printf("\n");
+        }
+       printf("\n");  
+    }
+
+    return 0;
+}

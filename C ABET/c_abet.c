22d51e393c616be4e8f1ff1a513b8d976da4973b
David Lara <david.lara.moreira@udla.edu.ec>
2023-06-05 11:54:21 -0500

    Primer commit

---
+++ C ABET/c_abet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+int año, carrera, alumnos;
+printf("Ingrese la cantidad de alumnos: ");
+scanf("%d", &alumnos);
+printf("Ingrese el año de ingreso de los alumnos: ");
+scanf("%d", &año);
+printf("Ingrese en que carrera necesita: ");
+scanf("%d", &carrera);
+
+    return 0;
+}
